43f0f8b8fef24794f8a34d4f7aa89edc06e534aa
Artyom-Gerchik <bombruchelyator@gmail.com>
2023-04-15 05:04:05 +0300

    seems like done, but H determinator is 0, idk is it right

--- LAB6/main.py
+++ LAB6/main.py
@@ -38,12 +38,18 @@ def quadratic_programming(x_with_T, J_with_b, J_with_b_with_star, A, D, c):
         H = numpy.zeros(((len(D_with_star) + len(A_B_with_star)), (len(D_with_star) + len(A_B_with_star))))
 
         H[:len(D_with_star), :len(D_with_star)] = D_with_star
-        H[:len(A_B_with_star), len(D_with_star):] = numpy.transpose(A_B_with_star)
-        H[len(D_with_star):, :len(A_B_with_star)] = A_B_with_star
+        H[:A_B_with_star.shape[1], len(D_with_star):] = numpy.transpose(A_B_with_star)
+        H[len(D_with_star):, :A_B_with_star.shape[1]] = A_B_with_star
 
         b_with_star = numpy.zeros((len(J_with_b_with_star) + len(A)))
         b_with_star[:len(J_with_b_with_star)] = D[J_with_b_with_star, j_0]
         b_with_star[len(J_with_b_with_star):] = A[:, j_0]
+
+        det = numpy.linalg.det(H)
+
+        if det == 0:
+            print('H DET IS 0')
+            return 
 
         x_vector = (numpy.dot(numpy.dot(-1, numpy.linalg.inv(H)), b_with_star))
 
@@ -53,9 +59,48 @@ def quadratic_programming(x_with_T, J_with_b, J_with_b_with_star, A, D, c):
                 l[i] = x_vector[counter]
                 counter += 1
 
-        print('sdg')
+        tettas = numpy.zeros((len(x_with_T)))
+        sigma = numpy.dot(numpy.dot(numpy.transpose(l), D), l)
 
-        break
+        if sigma > 0:
+            tettas[j_0] = abs(delta_x[j_0]) / sigma
+        else:
+            tettas[j_0] = numpy.inf
+
+        for index in J_with_b_with_star:
+            if l[index] < 0:
+                tettas[index] = -x_with_T[index] / l[index]
+            else:
+                tettas[index] = numpy.inf
+
+        tettas = tettas[:-1]
+
+        tetta_0 = min(tettas)
+
+        if tetta_0 == numpy.inf:
+            print(f'tetta_0 is : {tetta_0}')
+            return
+
+        index_to_remember_in_tettas = tettas.tolist().index(tetta_0)
+
+        x_with_T = x_with_T + numpy.dot(tetta_0, l)
+
+        if index_to_remember_in_tettas == j_0:
+            J_with_b_with_star = numpy.append(J_with_b_with_star, index_to_remember_in_tettas)
+        elif index_to_remember_in_tettas in J_with_b_with_star and index_to_remember_in_tettas not in J_with_b:
+            J_with_b_with_star = J_with_b_with_star[J_with_b_with_star != index_to_remember_in_tettas]
+        elif index_to_remember_in_tettas in J_with_b:
+            fourth = True
+            for j_plus in J_with_b_with_star:
+                if j_plus not in J_with_b:
+                    if numpy.dot(numpy.linalg.inv(A_B), A[:, j_plus])[
+                        J_with_b.tolist().index(index_to_remember_in_tettas)] != 0:
+                        J_with_b_with_star[J_with_b_with_star.tolist().index(index_to_remember_in_tettas)] = j_plus
+                        J_with_b = J_with_b[J_with_b != index_to_remember_in_tettas]
+                        fourth = False
+            if fourth:
+                J_with_b[J_with_b.tolist().index(index_to_remember_in_tettas)] = j_0
+                J_with_b_with_star[J_with_b_with_star.tolist().index(index_to_remember_in_tettas)] = j_0
 
 
 def main():
